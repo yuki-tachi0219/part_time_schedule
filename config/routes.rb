0cbcdc44b290ff9a9d7faa14b41b7fa4d594329b
taishi0202 <74192752+taishi0202@users.noreply.github.com>
2022-01-16 14:59:34 +0900

    #6 employee_controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   devise_for :employees, controllers: {
     sessions: 'employees/sessions'
   }
+
+  resources :employees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
