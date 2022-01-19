b4c547c8212d2941c86aaf3d021dbd17878e8057
ayumi kamada <dmrdmr58@gmail.com>
2022-01-19 23:41:39 +0900

    #a-3 シフト申請コントローラー作成

---
+++ app/controllers/attendance_requests_controller.rb
@@ -0,0 +1,39 @@
+class AttendanceRequestsController < ApplicationController
+
+  def index
+    @attendance_requests = AttendanceRequest.all
+  end
+
+  def new
+    @attendance_request = AttendanceRequest.new
+  end
+
+  def create
+    @attendance_request = AttendanceRequest.new(attendance_request_params)
+    @attendance_request.user_id = current_user.id
+    if params[:back]
+      render :new
+    else
+      if @attendance_request.save
+        redirect_to attendance_requests_path
+        render :new
+      end
+    end
+  end
+
+  def show
+    @attendance_request = AttendanceRequest.find(params[:id])
+  end
+
+  def edit
+    @attendance_request = AttendanceRequest.find(params[:id])
+  end
+
+  def update
+    @attendance_request = AttendanceRequest.find(params[:id])
+  end
+
+  def destroy
+    @attendance_request = AttendanceRequest.find(params[:id])
+  end
+end
