fe518e4e7d906112186f98c6891ce9488b124037
taishi0202 <74192752+taishi0202@users.noreply.github.com>
2022-01-26 10:14:12 +0900

    #9 deviseの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def configure_permitted_parameters
+    added_attrs = [ :last_name,
+                    :first_name,
+                    :email,
+                    :zipcode,
+                    :address,
+                    :phone_number,
+                    :password,
+                    :password_confirmation,
+                  ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 end
