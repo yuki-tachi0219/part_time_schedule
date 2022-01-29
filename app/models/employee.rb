b668e2706e68e5744d76d93ae009928597897d89
ayumi kamada <dmrdmr58@gmail.com>
2022-01-29 22:33:57 +0900

    feature/#a-10 employee.rbにリレーション追記

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,5 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :schedule
 end
