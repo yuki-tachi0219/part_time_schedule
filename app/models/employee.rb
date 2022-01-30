fad7a63d997a251361decb45b00b1df7f3f67d8e
ayumi kamada <dmrdmr58@gmail.com>
2022-01-30 21:04:27 +0900

    feature/#a-10 employee.rbに従業員削除時に勤怠も同時に削除されるよう追記

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,5 +3,5 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :schedule
+  has_many :schedule, dependent: :destroy
 end
