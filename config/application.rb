3ecdf73113a40d3bb3608d83ff47bafc5cedb4e2
Yuki.tachi <y.tachi0219@gmail.com>
2022-04-11 13:35:49 +0900

    enumの日本語化

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module PortfolioTest
     config.active_record.default_timezone = :local
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
