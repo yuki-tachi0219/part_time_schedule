cb2c2004f4291567961e132d29c0d928461d8244
Yuki.tachi <y.tachi0219@gmail.com>
2022-04-11 13:35:27 +0900

    モデルの記載ミス修正

--- app/models/absence_request.rb
+++ app/models/absence_request.rb
@@ -1,9 +1,14 @@
 class AbsenceRequest < ApplicationRecord
   belongs_to :schedule
+<<<<<<< HEAD
   has_many :absence_request_notifications
   enum state: {
     application: 0, #申請中
     approval: 1,  #承認
     rejection: 2  #却下
   }
+=======
+  has_many :absence_request_notifications, dependent: :destroy
+  has_many :notifications, through: :absence_request_notifications
+>>>>>>> cdfa58a (モデルの記載ミス修正)
 end
