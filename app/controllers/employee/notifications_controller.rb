26802c24596e516ec51efa8c990e76d2c48042fd
Yuki.tachi <y.tachi0219@gmail.com>
2022-04-11 13:34:52 +0900

    欠勤申請も考慮した通知絞り込み

--- app/controllers/employee/notifications_controller.rb
+++ app/controllers/employee/notifications_controller.rb
@@ -1,7 +1,10 @@
 class Employee::NotificationsController < ApplicationController
 
   def index
-    @notifications = Notification.joins(attendance_requests: :schedule).where(attendance_requests: { schedules: { employee_id: current_employee.id } }).distinct.where.not(action: "application")
+    relation = Notification.left_joins(attendance_requests: :schedule, absence_requests: :schedule)
+    @notifications = relation.where(attendance_requests: { schedules: { employee_id: current_employee.id } })
+                            .or(relation.where(absence_requests: { schedules: { employee_id: current_employee.id } }))
+                            .distinct.where.not(action: "application")
 
   end
 
