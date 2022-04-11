f9f905f710e215da1a9f120a0f257526d8bc4e4c
Yuki.tachi <y.tachi0219@gmail.com>
2022-04-11 14:10:54 +0900

    何の申請に関する通知なのかを表示

---
+++ app/helpers/notifications_helper.rb
@@ -0,0 +1,9 @@
+module NotificationsHelper
+
+  def associated_request(notification)
+    # views/ja.ymlをコントローラ構造に合わせて書いているため、t("administrator.notifications.index.request_type.attendance")をt(".request_type.attendance")に省略できる lazy_lookupというテクニック
+    notification.attendance_requests.present? ? t(".request_type.attendance") : t(".request_type.absence")
+  end
+
+end
+
