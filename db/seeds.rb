5c3cca9a6994a5108c7ad7bba3965d8bf5ffa7f2
ayumi kamada <dmrdmr58@gmail.com>
2022-04-17 00:22:24 +0900

    トランザクション処理を追加、中間テーブルのcreateメソッドを修正、インデント修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,98 +22,102 @@ Administrator.create!(
   delete_flg: false,
 )
 
-2.times do |n|
-  Schedule.create!(
-    starting_time: "2022-01-06 10:00:00",
-    closing_time: "2022-01-06 16:00:00",
-    employee_id: 1,
-  )
-end
-2.times do |n|
-  Schedule.create!(
-    starting_time: "2022-01-07 10:00:00",
-    closing_time: "2022-01-07 18:00:00",
-    employee_id: 2,
-  )
-end
-AttendanceRequest.create!(
-  state: 0,
-  schedule_id: 1,
-)
-AttendanceRequest.create!(
-  state: 0,
-  schedule_id: 2,
-)
-AttendanceRequest.create!(
-  state: 1,
-  schedule_id: 3,
-)
-AttendanceRequest.create!(
-  state: 2,
-  schedule_id: 4,
-)
-
-Notification.create!(
-  action: 0,
-  checked: false,
-)
-Notification.create!(
-  action: 0,
-  checked: false,
-)
-Notification.create!(
-  action: 1,
-  checked: false,
-)
-Notification.create!(
-  action: 2,
-  checked: false,
-)
-AttendanceRequest.all.ids.sort.each do |attendance_request_id|
-  Notification.all.ids.sort.each do |notification_id|
-    AttendanceRequestNotification.create(attendance_request_id: attendance_request_id, notification_id: notification_id)
+ActiveRecord::Base.transaction do
+  2.times do |n|
+    Schedule.create!(
+      starting_time: "2022-01-06 10:00:00",
+      closing_time: "2022-01-06 16:00:00",
+      employee_id: 1,
+    )
   end
-end
+  2.times do |n|
+    Schedule.create!(
+      starting_time: "2022-01-07 10:00:00",
+      closing_time: "2022-01-07 18:00:00",
+      employee_id: 2,
+    )
+  end
+  AttendanceRequest.create!(
+    state: 0,
+    schedule_id: 1,
+  )
+  AttendanceRequest.create!(
+    state: 0,
+    schedule_id: 2,
+  )
+  AttendanceRequest.create!(
+    state: 1,
+    schedule_id: 3,
+  )
+  AttendanceRequest.create!(
+    state: 2,
+    schedule_id: 4,
+  )
 
-AbsenceRequest.create!(
-  state: 0,
-  reason: "私用のため1",
-  schedule_id: 1,
-)
-AbsenceRequest.create!(
-  state: 0,
-  reason: "私用のため2",
-  schedule_id: 2,
-)
-AbsenceRequest.create!(
-  state: 1,
-  reason: "私用のため3",
-  schedule_id: 3,
-)
-AbsenceRequest.create!(
-  state: 2,
-  reason: "私用のため4",
-  schedule_id: 4,
-)
-
-Notification.create!(
-  action: 0,
-  checked: false,
-)
-Notification.create!(
-  action: 0,
-  checked: false,
-)
-Notification.create!(
-  action: 1,
-  checked: false,
-)
+  Notification.create!(
+    action: 0,
+    checked: false,
+  )
+  Notification.create!(
+    action: 0,
+    checked: false,
+  )
+  Notification.create!(
+    action: 1,
+    checked: false,
+  )
   Notification.create!(
     action: 2,
     checked: false,
   )
-AbsenceRequest.all.ids.sort.each do |absence_request_id|
-  Notification.all.ids.sort.each do |notification_id|
-    AbsenceRequestNotification.create(absence_request_id: absence_request_id, notification_id: notification_id)
+  AttendanceRequest.all.ids.sort.each do |attendance_request_id|
+    Notification.all.ids.sort.each do |notification_id|
+      AttendanceRequestNotification.create!(attendance_request_id: attendance_request_id, notification_id: notification_id)
+    end
+  end
+end
+
+ActiveRecord::Base.transaction do
+  AbsenceRequest.create!(
+    state: 0,
+    reason: "私用のため1",
+    schedule_id: 1,
+  )
+  AbsenceRequest.create!(
+    state: 0,
+    reason: "私用のため2",
+    schedule_id: 2,
+  )
+  AbsenceRequest.create!(
+    state: 1,
+    reason: "私用のため3",
+    schedule_id: 3,
+  )
+  AbsenceRequest.create!(
+    state: 2,
+    reason: "私用のため4",
+    schedule_id: 4,
+  )
+
+  Notification.create!(
+    action: 0,
+    checked: false,
+  )
+  Notification.create!(
+    action: 0,
+    checked: false,
+  )
+  Notification.create!(
+    action: 1,
+    checked: false,
+  )
+  Notification.create!(
+    action: 2,
+    checked: false,
+  )
+  AbsenceRequest.all.ids.sort.each do |absence_request_id|
+    Notification.all.ids.sort.each do |notification_id|
+      AbsenceRequestNotification.create!(absence_request_id: absence_request_id, notification_id: notification_id)
+    end
   end
 end
