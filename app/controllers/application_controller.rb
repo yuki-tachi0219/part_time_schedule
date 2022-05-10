0bfe7af102daf5996b1d3930617241f184a4ad31
ayumi kamada <dmrdmr58@gmail.com>
2022-05-11 00:34:47 +0900

    protectedからprivateへ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
 
     def configure_permitted_parameters
       added_attrs = [:last_name,
