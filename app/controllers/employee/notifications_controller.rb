1cafe21fd5121748ca4e43b4968f10c4455b0dec
ayumi kamada <dmrdmr58@gmail.com>
2022-05-11 00:47:03 +0900

    欠勤申請中の場合のリンク表示分岐追記

--- app/controllers/employee/notifications_controller.rb
+++ app/controllers/employee/notifications_controller.rb
@@ -1,7 +1,6 @@
 class Employee::NotificationsController < ApplicationController
   before_action :authenticate_employee!, only: %i[index]
 
-  # TODO: ログインユーザーごとに表示
   def index
     relation = Notification.left_joins(attendance_requests: :schedule, absence_requests: :schedule)
     @notifications = relation.where(attendance_requests: { schedules: { employee_id: current_employee.id } }).
