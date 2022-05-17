8e6a3ab52cc5444f59e170e2aab3cca2da29e26e
ayumi kamada <dmrdmr58@gmail.com>
2022-05-18 00:39:40 +0900

    承認処理時の通知レコード処理修正

--- app/controllers/administrator/absence_requests_controller.rb
+++ app/controllers/administrator/absence_requests_controller.rb
@@ -8,10 +8,11 @@ class Administrator::AbsenceRequestsController < ApplicationController
   end
 
   def update
+    binding.pry
     ActiveRecord::Base.transaction do
       absence_request = AbsenceRequest.find_by(id: params[:id])
       absence_request.update(absence_request_params)
-      absence_request.absence_request_notifications.create!(notification: Notification.new)
+      absence_request.absence_request_notifications.create!(notification_params)
       redirect_to administrator_absence_requests_path, notice: "欠勤申請編集が完了しました"
     end
   rescue
@@ -22,5 +23,11 @@ class Administrator::AbsenceRequestsController < ApplicationController
 
     def absence_request_params
       params.require(:absence_request).permit(:state)
+    end
+
+    def notification_params
+      notification = Notification.new(action: params[:absence_request][:state])
+
+      params.require(:notification).permit(:action).merge(action: notification)
     end
 end
